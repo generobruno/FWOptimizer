4f3ad43a505dd95fe52637f203dc745123560004
Luciano-P <luciano.peschiutta@unc.edu.ar>
2024-08-08 17:58:06 -0300

    All comments finished for chainComparator.py

--- fwoptimizer/utils/chainComparator.py
+++ fwoptimizer/utils/chainComparator.py
@@ -10,6 +10,7 @@ class ChainComparator:
     def reduceRedundancies(rules: List["ChainComparator.PseudoRule"], fieldList: FieldList) -> None:
         """
         Drop redundant PseudoRules of the 'rules' list.
+        The way to detect redundancies is to see if any of the rules is equal to the intersection between both.
 
         Args:
             rules: The list of PseudoRules to be reduced.
@@ -34,8 +35,15 @@ class ChainComparator:
                 i += 1
 
     class PseudoRule:
+        """
+        It is a class similar to Rule, but its predicates are objects instead of strings. 
+        Allows compare PseudoRules with each other.
+        """
 
         def __init__(self) -> None:
+            """
+            PseudoRule __init__.
+            """
 
             self._id = None
             self._decision = None
@@ -43,7 +51,7 @@ class ChainComparator:
 
         def __repr__(self) -> str:
             """
-            Rule __repr__
+            PseudoRule __repr__.
             
             Returns:
                 str: PseudoRule String representation
@@ -51,7 +59,13 @@ class ChainComparator:
             return f"PseudoRule {self._id}: {self._predicates} -> {self._decision}"
 
         def fillFromRule(self, rule: Rule, fieldList: FieldList):
-
+            """
+            Add to the PseudoRule the fields set in the fieldList, based on the values of the given rule.
+            
+            Args:
+                rule: Rule from which to take the values.
+                fieldList: The fieldlist that determines the fields that will be added.
+            """
             self._id = rule.getId()
             self._decision = rule.getDecision()
             self._predicates = {}
@@ -60,25 +74,68 @@ class ChainComparator:
                 element_list = rule.getOption(field.getName(), None)
                 self._predicates[field.getName()] = ElementSet.createElementSet(field.getType(), element_list if element_list else [])
         
-        def setId(self, id):
+        def setId(self, id) -> None:
+            """
+            Sets the id of the rule.
+
+            Args:
+                id: Id to set.
+            """
             self._id = id
 
-        def getId(self):
+        def getId(self) -> int:
+            """
+            Gets the id of the rule.
+
+            Returns:
+                int: The id of the rule.
+            """
             return self._id
 
-        def setDecision(self, decision):
+        def setDecision(self, decision: str) -> None:
+            """
+            Sets the decision of the rule.
+
+            Args:
+                decision: Decision to set.
+            """
             self._decision = decision
 
-        def getDecision(self):
+        def getDecision(self) -> str:
+            """
+            Gets the decision of the rule.
+
+            Returns:
+                str: The decision of the rule.
+            """
             return self._decision
         
-        def setPredicate(self, key, value):
+        def setPredicate(self, key, value) -> None:
+            """
+            Sets a predicate for the rule.
+
+            Args:
+                key: name of the predicate
+                value: ElementSet that represent the value for the predicate.
+            """
             self._predicates[key] = value
 
-        def getPredicates(self):
+        def getPredicates(self) -> dict:
+            """
+            Gets the dict of predicates for the rule.
+
+            Returns:
+                A Dict containing the predicates for the rule
+            """
             return self._predicates
         
-        def replicate(self):
+        def replicate(self) -> "ChainComparator.PseudoRule":
+            """
+            Gets a replica of this object.
+
+            Returns:
+                PseudoRule: A replica of this object.
+            """
             rep = ChainComparator.PseudoRule()
             rep.setId(self.getId())
             rep.setDecision(self.getDecision())
@@ -86,23 +143,46 @@ class ChainComparator:
                 rep.setPredicate(key, self.getPredicates()[key].replicate())
             return rep
         
-        def sameFieldValues(self, other: "ChainComparator.PseudoRule"):
-            """summary
+        def sameFieldValues(self, other: "ChainComparator.PseudoRule") -> bool:
+            """
+            Check if the 'other' PseudoRule have the same values for the predicates in self.
+            Do not check if 'other' have more predicates.
+
+            Args:
+                other: PseudoRule to compare.
+
+            Returns:
+                True if self and other have the same values. False otherwise.
             """
             for key in self.getPredicates():
                 if self.getPredicates()[key] != other.getPredicates()[key]:
                     return False
             return True
 
-        def isNull(self):
-
+        def isNull(self) -> bool:
+            """
+            Check if this PseudoRule is Null.
+            This condition occurs when one of the elementSet does not contain any value.         
+            
+            Returns:
+                True if self is Null. False otherwise.
+            """
             for key in self.getPredicates():
                 if self.getPredicates()[key].isEmpty():
                     return True
             return False
 
-        def intersection(self, other: "ChainComparator.PseudoRule", fieldList: FieldList):
+        def intersection(self, other: "ChainComparator.PseudoRule", fieldList: FieldList) -> "ChainComparator.PseudoRule":
+            """
+            Gets a new PseudoRule contains the intersection beetwen self and other.
 
+            Args:
+                other: The PseudoRule to compare.
+                fieldList: The fieldlist that determines the fields that will be compared.
+
+            Returns:
+                PseudoRule with intersection beetwen the rules if exist. None otherwise.
+            """
             result = ChainComparator.PseudoRule()
 
             for field in fieldList.getFields():
@@ -151,23 +231,36 @@ class ChainComparator:
             return diff
     
     class PseudoChain:
+        """
+        It is a class similar to Chain, but its rules are PseudoRules. 
+        """
 
         def __init__(self) -> None:
+            """
+            PseudoChain __init__.
+            """
             self._name = None
             self._defaultdecision = None
             self._rules = []
 
         def __repr__(self) -> str:
             """
-            Chain __repr__
+            PseudoChain __repr__.
 
             Returns:
-                str: Chain String representation
+                str: PseudoChain String representation
             """
             rules_str = "\n".join([str(rule) for rule in self._rules])
             return f"{self._name}:\n{rules_str if rules_str else ''}"
         
-        def fillFromChain(self, chain: Chain, fieldList: FieldList):
+        def fillFromChain(self, chain: Chain, fieldList: FieldList) -> None:
+            """
+            Add to the PseudoChain the PseudoRules set in the fieldList, based on the Rules of the given chain.
+            
+            Args:
+                chain: Chain from which to take the values.
+                fieldList: The fieldlist that determines the fields that will be added toi PseudoRules.
+            """
             self._name = chain.getName()
             self._defaultdecision = chain.getDefaultDecision()
             self._rules = []
@@ -177,26 +270,67 @@ class ChainComparator:
                 aux.fillFromRule(rule, fieldList)
                 self._rules.append(aux)
 
-        def addRule(self, rule):
+        def addRule(self, rule: "ChainComparator.PseudoRule") -> None:
+            """
+            Add the given PseudoRule to list of Rules.
+
+            Args:
+                rule: PseudoRule to add.
+            """
             self._rules.append(rule)
 
-        def getRules(self):
+        def getRules(self) -> List["ChainComparator.PseudoRule"]:
+            """
+            Gets the list of rules of the chain.
+            
+            Returns:
+                List of PseudoRules.
+            """
             return self._rules
 
-        def setName(self, name):
+        def setName(self, name) -> None:
+            """
+            Sets the name of the chain.
+
+            Args:
+                name: Name to set.
+            """
             self._name = name
         
         def getName(self):
+            """
+            Gets the nome of the chain.
+            
+            Returns:
+                str: Name of the chain.
+            """
             return self._name
         
-        def setDefaultDecision(self, defaultDecision):
+        def setDefaultDecision(self, defaultDecision) -> None:
+            """
+            Sets the default decision for the chain.
+            
+            Args:
+                defaultDecision: Decision to set.
+            """
             self._defaultdecision = defaultDecision
         
-        def getDefaultDecision(self):
+        def getDefaultDecision(self) -> str:
+            """
+            Gets the default decision for the chain.
+            
+            Returns:
+                The default  decision for the chain.
+            """
             return self._defaultdecision
         
-        def replicate(self):
+        def replicate(self) -> "ChainComparator.PseudoChain":
+            """
+            Gets a replica of this object.
 
+            Returns:
+                PseudoChain: A replica of this object.
+            """
             rep = ChainComparator.PseudoChain()
             rep.setName(self.getName())
             rep.setDefaultDecision(self.getDefaultDecision())
@@ -276,6 +410,9 @@ class ChainComparator:
 
 
     def __init__(self, fieldList: FieldList) -> None:
+        """
+        ChainComparator __init__.
+        """
         
         self._fieldList = fieldList
         self._chain1 = None
@@ -284,19 +421,44 @@ class ChainComparator:
         self._effectiveChain2 = None
     
     def __repr__(self) -> str:
-        
+        """
+        ChainComparator __repr__.
+
+        Returns:
+            str: ChainComparator String representation
+        """
         return f"CHAIN COMPARATOR\n\nORIGINALS\n\n{self._chain1}\n\n{self._chain2}\
                 \n\nCONVERTED\n\n{self._effectiveChain1}\n\n{self._effectiveChain2}"
     
-    def setFieldList(self, fieldList: FieldList):
+    def setFieldList(self, fieldList: FieldList) -> None:
+        """
+        Set the FieldList used for ChainComparator operations.
+        
+        Args:
+            fieldList: FieldList to use.
+        """
         self._fieldList = fieldList
     
-    def setChain1FromChain(self, chain1: Chain):
+    def setChain1FromChain(self, chain1: Chain) -> None:
+        """
+        Sets the chain1 PseudoChain from the given chain values.
+        Gets and save his effective form as well.
+        
+        Args:
+            chain: Chain to use for generate if correspondient PseudoChain. 
+        """
         self._chain1 = ChainComparator.PseudoChain()
         self._chain1.fillFromChain(chain1, self._fieldList)
         self._effectiveChain1 = self._chain1.getEffectiveChain(self._fieldList)
 
     def setChain2FromChain(self, chain2: Chain):
+        """
+        Sets the chain2 PseudoChain from the given chain values.
+        Gets and save his effective form as well.
+        
+        Args:
+            chain: Chain to use for generate if correspondient PseudoChain. 
+        """
         self._chain2 = ChainComparator.PseudoChain()
         self._chain2.fillFromChain(chain2, self._fieldList)
         self._effectiveChain2 = self._chain2.getEffectiveChain(self._fieldList)
